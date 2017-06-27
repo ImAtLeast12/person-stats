3ee0664c778e5be074de8e2f72f8a77cd4d5d061
Nicholas Loyd <Imatleast12@gmail.com>
2017-06-27 10:27:35 -0400

    this is a test

---
+++ README.md
@@ -0,0 +1,15 @@
+## Day 1 Homework
+*update the new stats div witht the value of the text inpt
+
+### bonus credit
+
+* Add the name to a paragraph inside the stats div
+
+### super mega bonus credit
+add another input to the form
+display the value of that input alongside the name in the 'div' or 'p'
+
+### super mega bonus credit hyper fighting
+change the apparance of the paragraph based on the value from the from
+
+turn the text blue if the user types blue in the form field
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-console.log('It worked!')
+/*function changeHeading(ev){
+    ev.preventDefault()
+    const f = ev.target
+    
+    const heading = document.querySelector('hi')
+    heading.textContent='Clicked!'
+}
+const personForm = document.querySelector('#peson-form')
+personForm.addEventListener('submit',changeHeading)*/
+
 
 function changeHeading(ev){
-    debugger
-    ev.target.textContent = 'Clickity click'
+    ev.preventDefault()
+    //const heading = document.querySelector('h1') 
+    
+    const input = document.querySelector('input') //find the input box
+    const input2 = document.querySelector('#visitorNumber')
+    const spam = document.querySelector('#Spam-message')
+    spam.textContent = 'Welcome ' + input.value + ' you are our ' +input2.value + ordinalIndicator(input2.value) +' visitor please send in $100.00, to claim a fabulous prize.'
 }
+//addEventListener('click',changeHeading)
+personform.addEventListener("submit",changeHeading)
 
-const btn = document.querySelector('button')
-btn.addEventListener('click',changeHeading)
 
-const p = document.querySelector('#instructions')
-p.addEventListener ('click',changeHeading)
+//So I am going to see if the last numbers where 1st, 2nd, 3rd otherwise it will return th
+function ordinalIndicator(visitorNumber){
+    var findOrdinal = visitorNumber.slice(-1)
+    if(findOrdinal == 1)
+        return "st"
+    else if (findOrdinal == 2)
+        return "nd"
+    else if (findOrdinal == 3)
+        return "rd"
+    else 
+        return "th"
+}
+//so what I want to do is see what is in the text box or INPUT
+//then make that be what gets changed
