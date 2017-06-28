d733245631a3b7ecfe4c26292b8b060cfe54d144
ImAtLeast12 <imatleast12@gmail.com>
2017-06-28 13:32:21 -0400

    Refactoring to consolidate the functions

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
+function handleSubmit(ev) {
+  ev.preventDefault()
 
-function changeHeading(ev){
-    ev.preventDefault()
-    //const heading = document.querySelector('h1') 
-    const input = document.querySelector('input') //find the input box
-    inputText = input.value.toLowerCase
-    const input2 = document.querySelector('#visitorNumber')
-    const spam = document.querySelector('#Spam-message')
-    spam.textContent = 'Welcome ' + input.value + ' you are our ' +input2.value + ordinalIndicator(input2.value) +' visitor please send in $100.00, to claim a fabulous prize.'
-    
-    try{
-        spam.style.color = input.value;
-    }
-    catch(err){
-    }
+  const list = renderList(ev)
+
+  // div.innerHTML = '<p>' + name + ', age ' + age + '</p>'
+  // div.innerHTML = `<p style="background-color: ${color}">${name}, age ${age}</p>`
 }
-personform.addEventListener("submit",changeHeading)
-//So I am going to see if the last numbers where 1st, 2nd, 3rd otherwise it will return th
-function ordinalIndicator(visitorNumber){
-    var findOrdinal = visitorNumber.slice(-1)
-    if(findOrdinal == 1)
-        return "st"
-    else if (findOrdinal == 2)
-        return "nd"
-    else if (findOrdinal == 3)
-        return "rd"
-    else 
-        return "th"
+
+function rederItem(item,text){
+    const itemD = document.createElement('li')
+    itemD.textContent = `${text}: ${item}`
+    return itemD
+}
+
+function renderColor(color,text){
+    const colorD = document.createElement('li')
+    colorD.style.backgroundColor = color
+    colorD.style.width = '6rem'
+    colorD.style.height = '3rem'
+    return colorD
+}
+
+const personForm = document.querySelector('#person-form')
+personForm.addEventListener('submit', handleSubmit)
+
+
+function renderList(ev){
+    const f = ev.target
+    const name = f.personName.value
+    const age = f.personAge.value
+    const color = f.favoriteColor.value
+
+    const div = document.querySelector('#stats')
+    const list = document.createElement('ul')
+
+    div.appendChild(list)
+
+    list.appendChild(rederItem(name,"Name"))
+    list.appendChild(rederItem(age,"Age"))
+    list.appendChild(renderColor(color,"Favorite color"))
+
+    return list
 }
